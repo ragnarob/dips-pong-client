ed80792a18df16a6bdf24aaed04e1a6697f62125
ragnarob <ragnarob@protonmail.com>
2019-09-04 03:11:12 +0200

    oops

--- src/api/gameApi.js
+++ src/api/gameApi.js
@@ -1,6 +1,8 @@
+const baseUrl = 'http://localhost:8081/api'
+
 export default {
   async addGame (winningPlayerId, losingPlayerId) {
-    let response = await fetch('/games', {
+    let response = await fetch(baseUrl + '/games', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({winnerId: winningPlayerId, loserId: losingPlayerId})
