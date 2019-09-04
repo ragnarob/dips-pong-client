cf7a9d275b2a540b6923ac6726a22aac5a903f65
ragnarob <ragnarob@protonmail.com>
2019-09-04 03:08:12 +0200

    Update api default path

--- src/api/gameApi.js
+++ src/api/gameApi.js
@@ -1,8 +1,6 @@
-const baseUrl = 'http://localhost:8081/api'
-
 export default {
   async addGame (winningPlayerId, losingPlayerId) {
-    let response = await fetch(baseUrl + '/games', {
+    let response = await fetch('/games', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({winnerId: winningPlayerId, loserId: losingPlayerId})
--- src/api/playerApi.js
+++ src/api/playerApi.js
@@ -1,13 +1,11 @@
-const baseUrl = 'http://localhost:8081/api'
-
 export default {
   async getPlayerList () {
-    let response = await fetch(baseUrl + '/players')
+    let response = await fetch('/players')
     return await response.json()
   },
 
   async addPlayer (playerName) {
-    let response = await fetch(baseUrl + '/players', {
+    let response = await fetch('/players', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({newPlayerName: playerName})
