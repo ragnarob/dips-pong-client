64322746f96d0f80e051195f76af5a63636ae19e
ragnarob <ragnarob@protonmail.com>
2019-09-11 12:30:37 +0200

    Attempt to offline-cache /api/players

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,19 @@
 if (workbox) {
-  console.log(`Workbox is loaded`);
+  console.log('Workbox is loaded')
 
-  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
 
+  workbox.precaching.precacheAndRoute(self.__precacheManifest)
+
+  workbox.routing.registerRoute(
+    /\.js$/,
+  )
+
+  workbox.routing.registerRoute(
+    new RegExp('/api/players'),
+    new workbox.strategies.StaleWhileRevalidate()
+  )
 } 
 else {
-  console.log(`Workbox didn't load`);
+  console.log('Workbox failed to load')
 }
